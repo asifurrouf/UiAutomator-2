4541e0a691d5046238e42e708cff68e8a65aeafb
jiahuixing <jiahuixing@gmail.com>
2013-07-04 20:32:15 +0800

    播放器

---
+++ src/com/miui/player/test/VideoPlayerTest.java
@@ -0,0 +1,228 @@
+package com.miui.player.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jiahuixing
+ * Date: 13-6-25
+ * Time: 下午3:50
+ * To change this template use File | Settings | File Templates.
+ */
+
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import com.android.uiautomator.core.UiDevice;
+import com.android.uiautomator.core.UiObject;
+import com.android.uiautomator.core.UiSelector;
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+import com.android.uiautomator.core.UiObjectNotFoundException;
+
+import java.util.Calendar;
+import java.io.IOException;
+
+import static java.lang.String.format;
+
+
+public class VideoPlayerTest extends UiAutomatorTestCase{
+
+    protected UiDevice device = null;
+
+    private static final int TEST_TIMES = 30;
+    private static final String LOG_TAG = "MIUI_PlayerTest";
+    private static final String VIDEO_PAC_NAME = "com.miui.video";
+    private static final String PLAYER_PAC_NAME = "com.miui.videoplayer";
+    private static final int SWIPE_STEPS = 20;
+    private int width;
+    private int height;
+
+    private static final int P_480 = 0;
+    private static final int P_720 = 1;
+    private static final int P_1080 = 2;
+
+    private static final String TOP = "Top";
+    private static final String BOTTOM = "Bottom";
+    private static final String LEFT = "Left";
+    private static final String RIGHT = "Right";
+    private static final String CENTRAL = "Central";
+
+    private static final String ZERO = "Zero";
+    private static final String NOT_ZERO = "Not_Zero";
+
+    private static final int[][] unlock_start_point= {
+            {240,360,540,},
+            {920,920,920,},
+    };
+    private static final int[][] unlock_end_point= {
+            {240,360,540,},
+            {1180,1180,1180,},
+    };
+
+
+    protected void setUp() throws Exception {
+        /*setUp*/
+        debug("setUp",1);
+        super.setUp();
+        device = getUiDevice();
+        width = device.getDisplayWidth();
+        height = device.getDisplayHeight();
+        debug("width=" + width + " height=" + height,1);
+        wakePhone();
+
+    }
+
+    private void debug(String msg,int wrap){
+        /*打印信息*/
+        if (wrap==1){
+            System.out.println("<"+msg+">");
+        }
+        else {
+            System.out.print(" <"+msg+"> ");
+        }
+    }
+
+    public void lockPhone() throws RemoteException {
+        /*锁屏*/
+        debug("lockPhone",1);
+        if (device.isScreenOn())
+            device.sleep();
+        sleep(2000);
+    }
+
+
+    public void wakePhone() throws RemoteException {
+        /*唤醒手机*/
+        debug("wakePhone",1);
+        device.wakeUp();
+        sleep(3000);
+    }
+
+    public void unlockPhone() throws RemoteException {
+        /*解锁*/
+        debug("unlockPhone",1);
+        int phone_type = phoneType();
+        int start_x = 0;
+        int start_y = 0;
+        int end_x = 0;
+        int end_y = 0;
+        start_x = unlock_start_point[0][phone_type];
+        start_y = unlock_start_point[1][phone_type];
+        end_x = unlock_end_point[0][phone_type];
+        end_y = unlock_end_point[1][phone_type];
+        /*debug("swipe_point="+","+start_x+","+start_y+","+end_x+","+end_y,1);*/
+        device.swipe(start_x, start_y, end_x, end_y, SWIPE_STEPS);
+        sleep(2000);
+    }
+
+    private int phoneType(){
+        /*手机分辨率类型*/
+        debug("phoneType",1);
+        int phone_type = P_720;
+        if (width == 720 && height == 1280) {
+            phone_type =  P_720;
+        }
+        else if ((width == 480 && height == 800)||(width == 480 && height == 854)){
+            phone_type =  P_480;
+        }
+        else if (width == 1080 && height == 1920){
+            phone_type = P_1080;
+        }
+        /*debug("phone_type="+phone_type,1);*/
+        return phone_type;
+    }
+
+    public void swipePhone(String type,int times){
+        /*滑动手机屏幕*/
+        debug(String.format("swipePhone:%s----Times:%d", type, times),1);
+        int start_x = 0;
+        int start_y = 0;
+        int end_x = 0;
+        int end_y = 0;
+        if (type.equals(TOP)) {
+            start_x = width / 2;
+            start_y = height*2 / 3;
+            end_x = width / 3;
+            end_y = height / 4;
+        }
+        else if (type.equals(BOTTOM)) {
+            start_x = width / 2;
+            start_y = height / 2;
+            end_x = width / 2;
+            end_y = height*3 / 4;
+        }
+        else if (type.equals(LEFT)) {
+            start_x = width*5 / 6;
+            start_y = height / 2;
+            end_x = width / 6;
+            end_y = height / 2;
+        }
+        else if (type.equals(RIGHT)) {
+            start_x = width / 3;
+            start_y = height*2 / 3;
+            end_x = width*2 / 3;
+            end_y = height*2 / 3;
+        }
+        else if (type.equals(CENTRAL)) {
+            start_x = width / 2;
+            start_y = height / 2;
+            end_x = width / 2;
+            end_y = height / 2;
+        }
+        /*debug("point="+start_x+","+start_y+","+end_x+","+end_y,1);*/
+        for (int j = 0; j < times ; j++) {
+            device.swipe(start_x, start_y, end_x, end_y, SWIPE_STEPS);
+        }
+        sleep(2000);
+    }
+
+    public void killVideo() throws IOException {
+        /*杀掉视频*/
+        debug("killVideo",1);
+        String kill = "am kill " + VIDEO_PAC_NAME;
+        String force_stop = "am force-stop " + VIDEO_PAC_NAME;
+        Runtime.getRuntime().exec(kill);
+        Runtime.getRuntime().exec(force_stop);
+        sleep(2000);
+    }
+
+    public void launchVideo() throws IOException {
+        /*打开视频*/
+        debug("launchVideo",1);
+        String launch = "am start -n com.miui.video/.HomeActivity";
+        Runtime.getRuntime().exec(launch);
+        sleep(2000);
+    }
+
+    public int randomIndex(int area,String type){
+        /*获取随机数*/
+        int rnd;
+        rnd = (int) (Math.random() * area);
+        if (type.equals(NOT_ZERO)){
+            debug(String.format("randomIndex>%s>type>%s",rnd,type),1);
+            if (rnd==0) {
+                debug("ReRandom>>>",0);
+                return randomIndex(area, type);
+            }
+            else {
+                return rnd;
+            }
+        }else if (type.equals(ZERO)){
+            debug(String.format("randomIndex>%s>type>%s",rnd,type),1);
+        }
+        return rnd;
+    }
+
+    private void waitMsg(String wait,int timeout){
+        /*打印等待信息*/
+        debug(wait,1);
+        int length;
+        length = timeout / 1000;
+        for (int i = 0; i < length;i++){
+            debug(String.format("%d", length - i),0);
+            /*System.out.print(""+(length-i)+" ");*/
+            sleep(1000);
+        }
+        debug("Next.",1);
+    }
+
+}
