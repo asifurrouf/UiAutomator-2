4da155e46987516ca5b72d044e9b8047387e4ff4
jiahuixing <jiahuixing@gmail.com>
2013-07-25 18:30:54 +0800

    flash.sh

--- flash.sh
+++ flash.sh
@@ -3,20 +3,36 @@ echo "\$#="$#
 if [[ $#>0 ]]; then
 	#statements
 	flash="$1"
-	if [[ -f "$flash" ]]; then
+	echo "$flash"
+	file_prefix=${flash%.*}
+	echo "$file_prefix"
+	if [[ "${flash##*.}" = "tar" ]]; then
 		#statements
-		if [ "${flash##*.}" = "sh" ]; then
-			echo "3"
-			chmod a+x "$flash"
-			adb reboot bootloader
-			./"$flash"			
+		tar xvf "$flash"
+		flash_shell="$file_prefix/flash_all_except_storage.sh"
+		echo "flash_shell=$flash_shell"
+		if [[ -f "$flash_shell" ]]; then
+			#statements
+			if [[ "${flash_shell##*.}" = "sh" ]]; then
+				#statements
+				chmod a+x "$flash_shell"
+				adb reboot bootloader
+				sleep 1
+				./"$flash_shell"
+				echo "delete tar file"
+				rm "$flash"	
+				echo "delete folder"
+				rm -r "$flash_shell"
+				echo "flash done"			
+			else
+				echo "not a  shell file"
+			fi			
 		else
-			echo "4"
+			echo "file not exist"
 		fi
-
 	else
-		echo "2"		
+		echo "not a tar file"
 	fi
 else
-	echo "1"
+	echo "need a argv"
 fi
